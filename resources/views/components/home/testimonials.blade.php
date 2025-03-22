7bcca02f94e620dade334c0e775ccc8a0fe73703
JesusMiguel03 <jmdefreitas10@gmail.com>
2025-03-22 10:31:55 -0400

    fix: remove unnecessary max-width restriction from testimonials content

--- resources/views/components/home/testimonials.blade.php
+++ resources/views/components/home/testimonials.blade.php
@@ -9,7 +9,7 @@
         <div class="testimonials-wrap owl-carousel owl-theme">
             @foreach ($comments as $comment)
                 <div class="single-testimonials">
-                    <p class="max-w-[30px]">"{{ $comment->content }}"</p>
+                    <p class="">"{{ $comment->content }}"</p>
                     <div class="testimonials-content">
                         <h4>{{ $comment->user->name }}</h4>
                     </div>
